ab10dab99429b4c10cca1603f299a1d70e85c80d
TJ Durnford <thdurnfo@microsoft.com>
2023-09-26 19:47:54 -0600

    minor changes

--- src/components/Details.tsx
+++ src/components/Details.tsx
@@ -1,4 +1,18 @@
+import { makeStyles, shorthands } from "@fluentui/react-components";
+
+const useStyles = makeStyles({
+  iframe: {
+    ...shorthands.border("none"),
+    width: "100%",
+    minWidth: "250px",
+    maxWidth: "100%",
+    height: "500px",
+    display: "block",
+  },
+});
+
 export const RaceDetails = () => {
+  const classes = useStyles();
   return (
     <section className="race-details">
       <h2>Race Details</h2>
@@ -15,6 +29,14 @@ export const RaceDetails = () => {
           <h3>Location</h3>
           <p>447 S Broadway</p>
           <p>Denver, CO 80209</p>
+        </div>
+        <div className="detail">
+          <h3>The Course</h3>
+          <iframe
+            title="TB50k2023 | 31.5 mi Running Route on Strava"
+            className={classes.iframe}
+            src="https://strava-embeds.com/route/3141754116069360858?fullWidth=true&hideElevation=true#ns=c94bd402-be4b-472d-b08b-f30264d430cb&hostOrigin=https%3A%2F%2Fwww.strava.com&hostPath=%2Froutes%2F3141754116069360858&hostTitle=TB50k2023+%7C+31.5+mi+Running+Route+on+Strava"
+          ></iframe>
         </div>
       </div>
     </section>
